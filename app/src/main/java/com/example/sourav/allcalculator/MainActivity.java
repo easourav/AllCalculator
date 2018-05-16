a5ebf0cf29d596066804fb1163234a1a4b1e49d7
easourav <37806949+easourav@users.noreply.github.com>
2018-05-17 03:00:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sourav/allcalculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sourav.allcalculator;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar=getSupportActionBar();
+        
+        actionBar.setTitle("Standart Calculator");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
